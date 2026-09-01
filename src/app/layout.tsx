3daea290bb301b25180f865c408c0114d2133810
Flua Gestão <desenvolvimento@fluagestao.com.br>
2026-08-31 22:22:47 -0300

    Remove modal e card antigo de onboarding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,6 @@ import "./mobile-admin.css";
 import { AdminPathSync } from "@/components/admin/AdminPathSync";
 import { MobileAdminNav } from "@/components/admin/MobileAdminNav";
 import { NovoPedidoRouteBridge } from "@/components/admin/NovoPedidoRouteBridge";
-import { OnboardingPrompt } from "@/components/admin/OnboardingPrompt";
 import { MarketingHeader } from "@/components/MarketingHeader";
 
 const geistSans = Geist({
@@ -84,7 +83,6 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
         <AdminPathSync />
         <NovoPedidoRouteBridge />
         <MarketingHeader />
-        <OnboardingPrompt />
         {children}
         <MobileAdminNav />
       </body>
